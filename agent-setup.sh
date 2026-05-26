53053a761aee3e3127bb37f5e641d5d846a547fc
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-05-26 13:03:40 +0530

    catalogue-cd-jenkis

--- agent-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo growpart /dev/nvme0n1 4
-sudo pvresize /dev/nvme0n1p4
-sudo lvextend -L +10G /dev/mapper/RootVG-homeVol
-sudo xfs_growfs /home
-sudo yum install fontconfig java-21-openjdk -y
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum install terraform -y 
